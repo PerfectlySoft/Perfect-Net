4cea26bc32577fb92a0b5366be6dad5b4359bc86
Kyle Jessup <badthing@gmail.com>
2017-01-12 18:49:56 -0500

    Resolving version diff

--- Package.swift
+++ Package.swift
@@ -24,9 +24,9 @@ let package = Package(
     name: "PerfectNet",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-COpenSSL-Linux.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-LinuxBridge.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", majorVersion: 2, minor: 0)
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-COpenSSL-Linux.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-LinuxBridge.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", majorVersion: 2)
     ],
     exclude: []
 )
@@ -35,8 +35,8 @@ let package = Package(
     name: "PerfectNet",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-COpenSSL.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", majorVersion: 2, minor: 0)
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-COpenSSL.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", majorVersion: 2)
     ],
     exclude: []
 )
